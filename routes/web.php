16aeb2a5eb0ede909a8ca648b191d47d5c182e51
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2022-08-26 02:58:59 +0200

    :construction: Performing store media request.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Api\MediaTrustupIo\Endpoints\Media;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
@@ -21,33 +22,48 @@ Route::get('/', function () {
 
 // fopen($request->file('file')->getPathname(), 'r')
 
-Route::post('upload', function(Request $request) {
-    // $response = Http::asMultipart()->post(env('MEDIA_URL'). '/api/form-test', [
-    //     'content' => $request->file('file')->get(),
-    //     'name' => $request->file('file')->getClientOriginalName()
-    // ]);
+Route::post('upload', function(Request $request, Media $mediaEnpoint) {
+    $response = Http::asMultipart()->post(env('MEDIA_URL'). '/api/media', [
+        // 'content' => $request->file('file')->get(),
+        'name' => $request->file('file')->getClientOriginalName()
+    ]);
+
+    // return $response->body();
+
+    $response = $mediaEnpoint->store($request->file('file'));
+
+    if ($response->failed()):
+        dd($response->error()->context());
+    endif;
+
+    return $response->response()->get();
+
+    // return $response->body();
+
+    // return $response->body();
     // // dd($response->json());
     // // return response(['response' => $response->json()]);
-    $response = Http::attach(
-        'tests[0][path]',
-        $request->file('file')->get(),
-        $request->file('file')->getClientOriginalName(),
-    )->asMultipart()->post(env('MEDIA_URL'). '/api/media', [
-        'model_id' => 25,
-        'model_type' => 'professional',
-        'collection' => 'images-strange-boyz',
-        'app_key' => 'messaging',
-        // 'files[0][path]' => $request->file('file')->get(),
-        // 'files[0][name]' => $request->file('file')->getClientOriginalName(),
-        'files[0][custom_properties][hello]' => 'Bonjour',
-        // 'files' => [
-        //     [
-        //         'path' => $request->file('file')->get(),
-        //         'name' => $request->file('file')->getClientOriginalName(),
-        //         'custom_properties' => ['test' => "being custom"]
-        //     ]
-        // ]
-    ]);
+    // $response = Http::attach(
+    //     'tests[0][path]',
+    //     $request->file('file')->get(),
+    //     $request->file('file')->getClientOriginalName(),
+    // )->asMultipart()->post(env('MEDIA_URL'). '/api/media', [
+    //     'model_id' => 25,
+    //     'model_type' => 'professional',
+    //     'collection' => 'images-strange-boyz',
+    //     'app_key' => 'messaging',
+    //     // 'files[0][path]' => $request->file('file')->get(),
+    //     // 'files[0][name]' => $request->file('file')->getClientOriginalName(),
+    //     'files[0][custom_properties][hello]' => 'Bonjour',
+    //     // 'files' => [
+    //     //     [
+    //     //         'path' => $request->file('file')->get(),
+    //     //         'name' => $request->file('file')->getClientOriginalName(),
+    //     //         'custom_properties' => ['test' => "being custom"]
+    //     //     ]
+    //     // ]
+    // ]);
+    
     // $response = Http::asMultipart()->post(env('MEDIA_URL'). '/api/media', [
     //     'model_id' => 25,
     //     'model_type' => 'professional',
@@ -63,5 +79,12 @@ Route::post('upload', function(Request $request) {
     //     //     ]
     //     // ]
     // ]);
-    return dd($response->body());
+    // return dd($response->body());
+
+    // $response = $mediaEnpoint->store($request->file('file'));
+
+    // if ($response->failed()):
+    //     dd($response->error()->context());
+    // endif;
+
 })->name('upload');
